4851897a858e2093b0b4d8fffc635a1de6470603
inesou01 <94458702+inesou01@users.noreply.github.com>
2023-11-28 19:38:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
-from retrying import retry
 import requests
 import pandas as pd
 import sys
@@ -10,93 +8,78 @@ from urllib.parse import quote
 from fake_useragent import UserAgent
 from concurrent.futures import ThreadPoolExecutor
 
-URL = "https://www.amazon.com/Pelican-Protector-Compatible-Anti-Scratch-Protection/dp/B0CC6NZYNW/ref=sr_1_1_sspa?crid=33SAYTHXN08T7&keywords=iphone+15+promax+case&qid=1700482546&sprefix=IPH%2Caps%2C1541&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15',
-    'Accept-Language': 'en-US, en;q=0.5'
-}
-
-
-# Créez une instance de UserAgent
+# Random user-agent
 ua = UserAgent()
 
+def scrap(URL):
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        'Accept-Language': 'en-US, en;q=0.5'
+    }
 
-webpage = requests.get(URL, headers=HEADERS)
-print(webpage.status_code)
-print(webpage.text)
+    webpage = requests.get(URL, headers=HEADERS)
+    print(webpage.status_code)
+   
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    reviews = []
+    link = []
+    for b in soup.findAll("a", {'data-hook': "see-all-reviews-link-foot"}):
+        link.append(b['href'])
+    print("links", link)
 
-soup = BeautifulSoup(webpage.content, "html.parser")
-reviews = []
-link = []
-for b in soup.findAll("a", {'data-hook': "see-all-reviews-link-foot"}):
-    link.append(b['href'])
-print("links", link)
+    CRAWLBASE_TOKEN = 'gAeK1ASL9vJyjT7iWKPxtw'  # Your Crawlbase token
 
-# Function to make a request using Crawlbase
-CRAWLBASE_TOKEN = 'gAeK1ASL9vJyjT7iWKPxtw'  # Your Crawlbase token
+    def make_request(url):
+        encoded_url = quote(url, safe='')
+        crawlbase_url = f'https://api.crawlbase.com/?token={CRAWLBASE_TOKEN}&url={encoded_url}'
+        response = requests.get(crawlbase_url, headers={'User-Agent': ua.random})
+        #response = requests.get(crawlbase_url, headers=HEADERS)
+        return response
 
-def make_request(url):
-    encoded_url = quote(url, safe='')
-    crawlbase_url = f'https://api.crawlbase.com/?token={CRAWLBASE_TOKEN}&url={encoded_url}'
-    response = requests.get(crawlbase_url, headers={'User-Agent': ua.random})
-    return response
-
-MAX_RETRIES = 2
-RETRY_SLEEP_SECONDS = 2
-
-def make_request_with_retry(url):
-    for attempt in range(MAX_RETRIES):
+    def process_page(url):
         page = make_request(url)
-
         if page.status_code == 200:
-            return page
+            print("Page", url, "fetched successfully")
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            new_reviews = [i.text for i in soup.findAll("span", {'data-hook': "review-body"})]
+            if not new_reviews:
+                print("No new reviews found on page", url)
+                return None
+
+            time.sleep(1)
+            return new_reviews
         else:
-            print(f"Error fetching page {url} - Attempt {attempt + 1} - Status Code: {page.status_code}")
-            time.sleep(RETRY_SLEEP_SECONDS)
-
-    print(f"Failed to fetch page {url} after {MAX_RETRIES} attempts.")
-    return None
-
-def process_page(url):
-    page = make_request_with_retry(url)
-
-    if page is not None and page.status_code == 200:
-        print("Page", url, "fetched successfully")
-        soup = BeautifulSoup(page.content, "html.parser")
-
-        new_reviews = [i.text for i in soup.findAll("span", {'data-hook': "review-body"})]
-        if not new_reviews:
-            print("No new reviews found on page", url)
+            print(f"Error fetching page {url} - Status Code: {page.status_code}")
             return None
 
-        time.sleep(random.uniform(2, 4))
-        return new_reviews
+    if link:
+        new_link = "https://www.amazon.com" + link[0]
+        urls = [new_link + f'&pageNumber={k}' for k in range(1, 11)]  # Adjust the range as needed
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            reviews = list(filter(None, executor.map(process_page, urls)))
+
+        if reviews:
+            reviews = [review for sublist in reviews for review in sublist]
+            rev = {'reviews': reviews}
+            review_data = pd.DataFrame.from_dict(rev)
+            review_data.to_csv("amazon_data.csv", header=True, index=False)
+            print("Total reviews collected:", len(reviews))
+        else:
+            print("No reviews collected.")
+            
     else:
-        return None
+        print("Error fetching initial page - Status Code:", webpage.status_code)
 
-if link:
-    new_link = "https://www.amazon.com" + link[0]
-    urls = [new_link + f'&pageNumber={k}' for k in range(1, 4)]  # Adjust the range as needed
+    print(review_data)
 
-    with ThreadPoolExecutor(max_workers=5) as executor:  # Adjust the number of workers as needed
-        reviews = list(filter(None, executor.map(process_page, urls)))
-
-    if reviews:
-        reviews = [review for sublist in reviews for review in sublist]
-        rev = {'reviews': reviews}
-        review_data = pd.DataFrame.from_dict(rev)
-        review_data.to_csv("amazon_data.csv", header=True, index=False)
-        print("Total reviews collected:", len(reviews))
+    if not review_data.empty:
+        review = review_data['reviews'].iloc[8]
+        print("Full text of the first review:\n", review)
     else:
-        print("No reviews collected.")
-else:
-    print("Error fetching initial page - Status Code:", webpage.status_code)
+        print("Aucune revue n'a été collectée.")
 
-print(review_data)
-
-# Afficher un review en entier
-if not review_data.empty:
-    review = review_data['reviews'].iloc[10]
-    print("Full text of the first review:\n", review)
-else:
-    print("Aucune revue n'a été collectée.")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    scrap(url)
