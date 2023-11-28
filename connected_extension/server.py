1bdbe8291ae69be1ea56d5941c2595e23fc27e07
inesou01 <94458702+inesou01@users.noreply.github.com>
2023-11-28 22:17:57 +0100

    Update server.py

--- connected_extension/server.py
+++ connected_extension/server.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import subprocess
+import json
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -12,15 +14,37 @@ CORS(app)
 @app.route('/')
 def index():
     return 'sentiment analysis app!'
+
+# Route pour envoyer les données JSON à l'extension
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    try:
+        json_path = os.path.join(os.path.dirname(__file__), 'donnees.json')
+        
+        # Vérifier si le fichier JSON existe
+        if not os.path.isfile(json_path):
+            raise FileNotFoundError("Le fichier JSON n'existe pas.")
+        
+        ## Charger les données à partir du fichier JSON
+        with open(json_path, 'r') as json_file:
+            data = json.load(json_file)
+            premier_pourcentage = data['VariablesIndependantes']['PourcentageAvisNegatifGlobal']
+            print(premier_pourcentage)
+        #print(data)
+        return jsonify(data)
+    
+    except Exception as e:
+        print(f"Erreur lors de la récupération des données : {e}")
+        return jsonify({"error": "Erreur lors de la récupération des données"}), 500
+    
+# Ajout du décorateur pour lier la fonction à la route /receive_data
 @app.route('/receive_data', methods=['POST'])
-   
 def receive_data():
     print("starting")
     url = request.json
     print(url)
-    subprocess.run(["python", "test.py", url])  # Run scrapping.py with the URL sent from the extension
+    subprocess.run(["python", "test2.py", url])  # Run scrapping.py with the URL sent from the extension
     return "Data Received"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
