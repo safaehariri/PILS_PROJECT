5ecd7e20880ffddc73552dd590f670400f61a6ac
inesou01 <94458702+inesou01@users.noreply.github.com>
2024-01-25 12:37:27 +0100

    Update contentscript.js

--- connected_extension/extension_connected/contentscript.js
+++ connected_extension/extension_connected/contentscript.js
@@ -20,13 +20,38 @@ function sendDataToServer(data) {
 		console.error('Error:', error);
 	});
 }
- 
 
+function fetchDataFromServer() {
+    fetch('http://127.0.0.1:5000/get_data', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Data from server:', data);
+            // Envoie les données récupérées depuis le serveur au popup
+            chrome.runtime.sendMessage({
+                from: 'content',
+                subject: 'sendDataToPopup',
+                data: data
+            });
+        })
+		
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+ 
 const currentUrl = window.location.href;
 chrome.runtime.onMessage.addListener(function(message, sender, response) {
     if (message.from === 'popup' && message.subject === 'getData') {
 
 		sendDataToServer(currentUrl);
+
+		// Envoie la requête pour récupérer les données du serveur
+        //fetchDataFromServer(); safae
         // Start the review scraping process
         
     }
