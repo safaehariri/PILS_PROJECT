4c47b377a1085b029ec4bdfdfb47f431b41ae76b
inesou01 <94458702+inesou01@users.noreply.github.com>
2024-01-25 12:44:45 +0100

    Update script.js

--- connected_extension/extension_connected/script.js
+++ connected_extension/extension_connected/script.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Add an event listener for the search button
+    const searchButton = document.getElementById('searchButton');
+    searchButton.addEventListener('click', function () {
+        handleSearch();
+    });
+
+    document.getElementById('aboutUsLink').addEventListener('click', function() {
+        var whoContent = document.getElementById('whoContent');
+        whoContent.style.display = (whoContent.style.display === 'none' || whoContent.style.display === '') ? 'block' : 'none'; });
+    
+        document.getElementById('helpLink').addEventListener('click', function() {
+            var whoContent = document.getElementById('helpContent');
+            whoContent.style.display = (whoContent.style.display === 'none' || whoContent.style.display === '') ? 'block' : 'none'; });
+            
     function fetchDataFromServer() {
         // Retourner la promesse générée par fetch
         return fetch('http://127.0.0.1:5000/get_data', {
@@ -18,26 +33,44 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function updatePopupHTML(data) {
+
+        var categoryDropdown = document.getElementById('categoryDropdown');
+
+        // Videz la liste déroulante existante
+        categoryDropdown.innerHTML = '';
+
+        // Ajoutez les options à la liste déroulante avec les noms des catégories
+        for (let i = 1; i <= 4; i++) {
+            const categoryName = data[`Categorie${i}`]['nom'];
+
+            // Créez un élément d'option
+            var option = document.createElement('option');
+            option.value = categoryName;
+            option.textContent = categoryName;
+
+            // Ajoutez l'option à la liste déroulante
+            categoryDropdown.appendChild(option);
+        }
         // Accédez aux pourcentages des variables indépendantes
-        const negatif = data['VariablesIndependantes']['PourcentageAvisNegatifGlobal']
+        const negatif = data['general_sentiments']['negative']
         document.getElementById('negatifValue').textContent = `${negatif}%`;
 
-        const positif = data['VariablesIndependantes']['PourcentageAvisPositifGlobal']
+        const positif = data['general_sentiments']['positive']
         document.getElementById('PositifValue').textContent = `${positif}%`;
 
-        const neutre = data['VariablesIndependantes']['PourcentageAvisNeutreGlobal']
-        document.getElementById('neutreValue').textContent = `${neutre}%`;
+        // const neutre = data['general']['PourcentageAvisNeutreGlobal']
+        // document.getElementById('neutreValue').textContent = `${neutre}%`;
 
             // Sélectionnez l'élément path par sa classe
         var cercleDeProgression1 = document.querySelector('.circle-red');
         var cercleDeProgression2 = document.querySelector('.circle-green');
-        var cercleDeProgression3 = document.querySelector('.circle-blue');
+        // var cercleDeProgression3 = document.querySelector('.circle-blue');
         
 
         // Mettez à jour la valeur de stroke-dasharray en fonction de la valeur dynamique
         cercleDeProgression1.setAttribute('stroke-dasharray', negatif + ', 100');
         cercleDeProgression2.setAttribute('stroke-dasharray', positif + ', 100');
-        cercleDeProgression3.setAttribute('stroke-dasharray', neutre + ', 100');
+        // cercleDeProgression3.setAttribute('stroke-dasharray', neutre + ', 100');
 
         //categories
 
@@ -93,8 +126,91 @@ document.addEventListener('DOMContentLoaded', function () {
         progressBar4.style.width = `${neg4}%`;
         progressBar4.textContent = `${neg4}%`;
         progressRemaining4.textContent = `${pos4}%`;
+
+        extractAndDisplayCommentsFromCategory(data['Categorie1'], 'comments_container_1', 'negCom');
+        extractAndDisplayCommentsFromCategory(data['Categorie1'], 'comments_container_1_pos', 'posCom');
+        extractAndDisplayCommentsFromCategory(data['Categorie2'], 'comments_container_2_pos', 'posCom');
+        extractAndDisplayCommentsFromCategory(data['Categorie2'], 'comments-container-2','negCom');
+        extractAndDisplayCommentsFromCategory(data['Categorie3'], 'comments_container_3_pos', 'posCom');
+        extractAndDisplayCommentsFromCategory(data['Categorie3'], 'comments-container-3','negCom');
+        extractAndDisplayCommentsFromCategory(data['Categorie4'], 'comments_container_4_pos','posCom');
+        extractAndDisplayCommentsFromCategory(data['Categorie4'], 'comments-container-4','negCom');
+        
     }
 
+     // Function to handle the search
+    function handleSearch() {
+        const searchTerm = document.getElementById('categoryDropdown').value.toLowerCase();
+
+        // Check if the search term matches any category name
+        for (let i = 1; i <= 4; i++) {
+            const categoryName = document.getElementById(`Categorie${i}`).textContent.toLowerCase();
+            const commentsContainerPos = document.getElementById(`comments_container_${i}_pos`);
+            const commentsContainerNeg = document.getElementById(`comments-container-${i}`);
+
+            if (categoryName.includes(searchTerm)) {
+                
+                scrollToElement(`Categorie${i}`);
+                return;
+            } 
+        }
+    }
+
+    function scrollToElement(elementId) {
+        document.getElementById(elementId).scrollIntoView({ behavior: 'smooth' });
+    }
+
+    function extractAndDisplayCommentsFromCategory(categoryData, containerId, com) {
+        const comments = categoryData[com];
+        const commentsContainer = document.getElementById(containerId);
+
+        const maxComments = 3; // Limite le nombre de commentaires à afficher initialement
+        const displayedComments = comments.slice(0, maxComments);
+
+
+        displayedComments.forEach(comment => {
+            const commentElement = document.createElement('p');
+            commentElement.textContent = `- ${comment}`;
+            commentsContainer.appendChild(commentElement);
+        });
+    
+        // Si le nombre total de commentaires est supérieur à la limite, ajoute le bouton "Read more reviews"
+        if (comments.length > maxComments) {
+            const readMoreButton = document.createElement('button');
+            readMoreButton.textContent = 'Read more reviews';
+
+            readMoreButton.style.marginTop = '10px'; // Exemple de marge en haut
+            readMoreButton.style.background = '#EBAB2B'; // Couleur de fond jaune-orange
+            readMoreButton.style.border = 'none'; // Supprimer la bordure
+            readMoreButton.style.borderRadius = '13.62px'; // Bouts arrondis
+            readMoreButton.style.color = '#000'; 
+            readMoreButton.style.marginTop = '10px'; // Exemple de marge en haut
+            readMoreButton.style.height = '30px';
+            readMoreButton.style.marginLeft= '360px';
+
+            readMoreButton.addEventListener('click', function () {
+                // Efface le contenu actuel du conteneur
+                if (com === "posCom") {
+                    commentsContainer.innerHTML = '<h4>Positive comments :</h4>';
+                } else {
+                    commentsContainer.innerHTML = '<h4>Negative comments :</h4>';
+                }
+                
+                // Affiche tous les commentaires lorsque le bouton est cliqué
+                comments.forEach(comment => {
+                    const commentElement = document.createElement('p');
+                    commentElement.textContent = `- ${comment}`;
+                    commentsContainer.appendChild(commentElement);
+                });
+    
+                // Cache le bouton "Read more reviews" après l'affichage complet
+                // readMoreButton.style.display = 'none';
+            });
+            commentsContainer.appendChild(readMoreButton);
+        }
+    }
+
+    
     // Appeler fetchDataFromServer et stocker le résultat dans storedData
     fetchDataFromServer().then(data => {
         
@@ -102,58 +218,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         
     });
+
+    
 });
 
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    // Fonction pour extraire et afficher les commentaires
-    function extractAndDisplayComments(csvFilePath, containerId) {
-        fetch(csvFilePath)
-            .then(response => response.text())
-            .then(data => {
-                const lines = data.split('\n');
-                const commentsContainer = document.getElementById(containerId);
-  
-                for (let i = 0; i < lines.length; i++) {
-                    const comment = lines[i].trim();
-  
-                    // Ignorer les lignes vides
-                    if (comment !== '') {
-                        // Créer un paragraphe pour chaque commentaire et l'ajouter au conteneur
-                        const commentElement = document.createElement('p');
-                        commentElement.textContent = comment;
-                        commentsContainer.appendChild(commentElement);
-                    }
-                }
-            })
-            .catch(error => console.error(`Erreur lors de la récupération du fichier CSV ${csvFilePath}`, error));
-    }
-  
-    // Utiliser la fonction pour chaque fichier CSV et chaque conteneur
-    extractAndDisplayComments('comments1.csv', 'comments_container_1');
-    extractAndDisplayComments('comments2.csv', 'comments-container-2');
-    extractAndDisplayComments('comments2.csv', 'comments-container-3');
-    extractAndDisplayComments('comments_neg.csv', 'comments_container_1_neg');
-    // Ajoutez autant d'appels à extractAndDisplayComments que nécessaire
-  });
-  
-  
-  // script.js
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const triggerElement = document.getElementById('triggerElement');
-    const comments_container_1 = document.getElementById('comments_container_1');
-    const comments_container_1_neg = document.getElementById('comments_container_1_neg');
-  
-    triggerElement.addEventListener('mouseover', function() {
-        comments_container_1.classList.add('hidden');
-        comments_container_1_neg.classList.remove('hidden');
-    });
-  
-    triggerElement.addEventListener('mouseout', function() {
-        comments_container_1.classList.remove('hidden');
-        comments_container_1_neg.classList.add('hidden');
-    });
-  });
-  
-  
+
